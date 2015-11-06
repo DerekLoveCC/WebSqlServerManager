93769233578fdd2bb5976c47a898359b3dc7fc48
DerekLoveCC <zhaomingbo1010@126.com>
2015-11-06 21:57:28 +0800

    add unit ioc

---
+++ SqlServer.Api/SqlServer.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Unity;
+using System.Web.Http;
+
+namespace SqlServer.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            RegisterUnityContainer(config);
+        }
+
+        private static void RegisterUnityContainer(HttpConfiguration config)
+        {
+            var container = new UnityContainer();
+            container.RegisterTypes(AllClasses.FromLoadedAssemblies(), WithMappings.FromAllInterfaces, WithName.TypeName, WithLifetime.Transient);
+            config.DependencyResolver = new UnityResolver(container);
+        }
+    }
+}
